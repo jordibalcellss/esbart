ce58734058415464f8f2361ba3c66b1840d2b0c4
jbalcells <jbalcells@laptop.test>
2024-06-06 17:35:17 +0100

    improve query
    using TIMESTAMPDIFF in hours instead of truncating

--- cron.php
+++ cron.php
@@ -7,7 +7,10 @@ ini_set('display_errors',DISPLAY_ERRORS);
 require 'include/functions.php';
 
 $db = new DB();
-$stmt = $db->prepare('UPDATE pw_set_requests SET expired=1 WHERE id IN (SELECT id FROM pw_set_requests WHERE expired=0 AND hour(timediff(now(),created))>'.TOKEN_EXPIRES_H.')');
+$stmt = $db->prepare('
+  UPDATE pw_set_requests SET expired = 1
+  WHERE NOT expired
+  AND TIMESTAMPDIFF(HOUR, created, NOW()) > '.TOKEN_EXPIRES_H);
 $stmt->execute();
 
 ?>
